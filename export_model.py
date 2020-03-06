7682ae2f2e504054a1e5fd5445106d84abfab871
Arron Cao <qqcao.cs@gmail.com>
2020-03-06 11:55:20 -0500

    update tflite model

--- export_model.py
+++ export_model.py
@@ -37,7 +37,7 @@ def create_model(bert_config, input_ids, segment_ids,input_mask, num_labels):
 def main(args):
     bert_config = modeling.BertConfig.from_json_file(args.bert_config_file)
     tf.gfile.MakeDirs(args.output_dir)
-
+    export_all = True if args.mode == 'all' else False
     with tf.Session() as sess:
       input_ids_ph = tf.placeholder(shape=[1, args.max_seq_length],
                                   dtype=tf.int32, name='input_ids')
@@ -47,38 +47,44 @@ def main(args):
     #                               dtype=tf.int32, name='input_mask')
       prob = create_model(bert_config, input_ids_ph, segment_ids_ph, None, args.num_labels)
       sess.run(tf.initializers.global_variables())
-      saver = tf.train.Saver(var_list=tf.global_variables())
-      print("saving model checkpoints...")
-      saved_model_path = os.path.join(args.output_dir, 'model')
-      saver.save(sess, saved_model_path)
 
-      print("exporting saved model...")
-      save_dir = os.path.join(args.output_dir, 'saved_model')
-      if tf.gfile.Exists(save_dir):
-        tf.gfile.DeleteRecursively(save_dir)
-      tf.saved_model.simple_save(sess, save_dir,
-                                inputs={'input_id': input_ids_ph,
-                                'segment_ids': segment_ids_ph,
-                                # 'input_mask': input_mask_ph,
-                                },
-                                outputs={'prob': prob})
+      if export_all or args.mode == 'ckpt':
+        print("saving model checkpoints...")
+        saver = tf.train.Saver(var_list=tf.global_variables())
+        saved_model_path = os.path.join(args.output_dir, 'model')
+        saver.save(sess, saved_model_path)
 
-      tflite_file = os.path.join(args.output_dir, 'model.tflite')
-      print("exporting tflite model...")
-      converter = tf.lite.TFLiteConverter.from_session(sess, [input_ids_ph, segment_ids_ph],
-                                                        [prob])
-      if args.quantize:
-          converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+      if export_all or args.mode == 'saved':
+        print("exporting saved model...")
+        save_dir = os.path.join(args.output_dir, 'saved_model')
+        if tf.gfile.Exists(save_dir):
+          tf.gfile.DeleteRecursively(save_dir)
+        tf.saved_model.simple_save(sess, save_dir,
+                                  inputs={'input_id': input_ids_ph,
+                                  'segment_ids': segment_ids_ph,
+                                  # 'input_mask': input_mask_ph,
+                                  },
+                                  outputs={'prob': prob})
+      if export_all or args.mode == 'lite':
+        tflite_file = os.path.join(args.output_dir, 'model.tflite')
+        print("exporting tflite model...")
+        converter = tf.lite.TFLiteConverter.from_session(sess, [input_ids_ph, segment_ids_ph],
+                                                          [prob])
+        if args.quantize:
+            converter.optimizations = [tf.lite.Optimize.DEFAULT]
+            # converter.inference_type = tf.lite.constants.QUANTIZED_UINT8
 
-      converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
-      tflite_model = converter.convert()
-      with open(tflite_file, "wb") as f:
-          f.write(tflite_model)
+        converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
+        tflite_model = converter.convert()
+        with open(tflite_file, "wb") as f:
+            f.write(tflite_model)
       
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--bert_config_file', type=str, default=None)
-    parser.add_argument("-o", "--output_dir", type=str, default='bert_classifier')
+    parser.add_argument("-o", "--output_dir", type=str, default='data/bert_base')
+    parser.add_argument("-m", "--mode", type=str, choices=("lite", "ckpt", "saved", "all"),
+                        default='all')
     parser.add_argument("-n", "--num_labels", type=int, default=2)
     parser.add_argument("-l", "--max_seq_length", type=int, default=None)
     parser.add_argument("-q", "--quantize", action='store_true', help="quantize the tflite model")
--- run_tflite.py
+++ run_tflite.py
@@ -12,12 +12,11 @@ EDGETPU_SHARED_LIB = {
   'Windows': 'edgetpu.dll'
 }[platform.system()]
 
-def make_interpreter(model_file):
+def make_interpreter(model_file, run_on_tpu=False):
   model_file, *device = model_file.split('@')
+  delegates = [tflite.load_delegate(EDGETPU_SHARED_LIB, {'device': device[0]} if device else {})] if run_on_tpu else None
   return tflite.Interpreter(model_path=model_file,
-      # To run on host, comment the line below
-      experimental_delegates=[tflite.load_delegate(EDGETPU_SHARED_LIB, {'device': device[0]} if device else {})]
-  )
+                            experimental_delegates=delegates)
 
 def main():
   parser = argparse.ArgumentParser(
@@ -26,10 +25,12 @@ def main():
       '-m', '--model', required=False, default='model.tflite',
        help='File path of .tflite file.')
   parser.add_argument("-l", "--max_seq_length", type=int, default=384)
-
+  parser.add_argument('-c', '--count', type=int, default=5,
+      help='Number of times to run inference')
+  parser.add_argument("-tpu", "--tpu", action='store_true', help="run on tpu")
   args = parser.parse_args()
 
-  interpreter = make_interpreter(args.model)
+  interpreter = make_interpreter(args.model, args.tpu)
   interpreter.allocate_tensors()
 
   input_details = interpreter.get_input_details()
@@ -39,14 +40,17 @@ def main():
 
   print(input_details)
   # Example output: [{'name': 'input_1', 'index': 0, 'shape': array([  1, 384]), 'dtype': <class 'numpy.int32'>, 'quantization': (0.0, 0), 'quantization_parameters': {'scales': array([], dtype=float32), 'zero_points': array([], dtype=int32), 'quantized_dimension': 0}}]
-
+  print('running on {}'.format('edge tpu' if args.tpu else 'host machine'))
   input_content = np.zeros([1, args.max_seq_length])
   input_data = np.array(input_content, dtype=input_type)
   interpreter.set_tensor(input_details[0]['index'], input_data)
+  for _ in range(args.count):
+    start = time.perf_counter()
+    interpreter.invoke()
+    inference_time = time.perf_counter() - start
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print('%.1fms' % (inference_time * 1000))
 
-  interpreter.invoke()
-
-  output_data = interpreter.get_tensor(output_details[0]['index'])
   print(output_data)
 
 if __name__ == '__main__':
