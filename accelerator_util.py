0cc1e60e0b790756d1fde6950e264a18342c3e39
Alexandru Irimiea <alexandru.irim@gmail.com>
2020-03-10 04:17:19 +0200

    Using thread pool to generate OpenVINO models

--- accelerator_util.py
+++ accelerator_util.py
@@ -9,6 +9,71 @@ import os
 # from openvino.inference_engine import IENetwork, IEPlugin
 from datetime import datetime
 
+def generate_models_info():
+  l_vocab_size = [30522] #30522
+  l_hidden_size = [768]
+  l_num_hidden_layers = [12]
+  l_num_attention_heads = [1]#, 2, 3, 4, 6, 8, 9, 12, 16, 24, 32]
+  l_intermediate_size = [3072] #3072
+  l_input_size = [384]
+
+  return generate_models_info_base(l_vocab_size,
+                          l_hidden_size,
+                          l_num_hidden_layers,
+                          l_num_attention_heads,
+                          l_intermediate_size,
+                          l_input_size)
+
+def generate_models_info_base(l_vocab_size,
+                          l_hidden_size,
+                          l_num_hidden_layers,
+                          l_num_attention_heads,
+                          l_intermediate_size,
+                          l_input_size):
+
+  models_info = []
+
+  for vocab_size in l_vocab_size:
+    for hidden_size in l_hidden_size:
+      for num_hidden_layers in l_num_hidden_layers:
+        for num_attention_heads in l_num_attention_heads:
+          for intermediate_size in l_intermediate_size:
+            for input_size in l_input_size:
+              model_info = {'vocab_size': vocab_size,
+                                'hidden_size': hidden_size,
+                                'num_hidden_layers': num_hidden_layers,
+                                'num_attention_heads': num_attention_heads,
+                                'intermediate_size': intermediate_size,
+                                'input_size': input_size,
+                                }
+              model_name = ''
+              results_file_header = ''
+              results_file_row = ''
+              for key, value in model_info.items():
+                model_name += key + '_' + str(value).zfill(4) + '_'
+                results_file_header += key + '\t'
+                results_file_row += str(value) + '\t'
+              model_info['model_name'] = model_name
+              model_info['model_meta_path'] = model_name + '/model.meta'
+              model_info['model_xml_path'] = model_name + '/model.xml'
+              model_info['results_file_header'] = results_file_header
+              model_info['results_file_row'] = results_file_row
+              models_info.append(model_info)
+
+  return models_info
+
+def make_dir(dir_path):
+  os.makedirs(dir_path, exist_ok=True)
+
+def make_parent_dir(dir_path):
+  make_dir(os.path.dirname(dir_path))
+
+def get_path_without_ext(file_path):
+  return os.path.splitext(file_path)[0]
+
+def replace_ext(file_path, new_ext):
+  return get_path_without_ext(file_path) + '.' + new_ext
+
 def get_timestamp():
   return datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 
--- generate_models.py
+++ generate_models.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import modeling
 import tensorflow as tf
+from multiprocessing import Pool, Queue, Process, Manager
+import signal
+import time
+from accelerator_util import *
 
 def create_model(bert_config, input_ids, segment_ids,input_mask, num_labels):
   """Creates a classification model."""
@@ -37,18 +41,20 @@ def create_model(bert_config, input_ids, segment_ids,input_mask, num_labels):
 # def main(args):
 #     bert_config = modeling.BertConfig.from_json_file(args.bert_config_file)
 def generate_model(output_dir,
-                hidden_size=512,
-                num_hidden_layers=6,
-                num_attention_layers=8,
+                vocab_size=30522,
+                hidden_size=768,
+                num_hidden_layers=12,
+                num_attention_heads=12,
                 intermediate_size=3072,
                 max_seq_length = 384,
                 num_labels = 2,
                 quantize = True):
     tf.reset_default_graph()
-    bert_config = modeling.BertConfig(30522, hidden_size, num_hidden_layers, num_attention_layers, intermediate_size)
-
-    # print(bert_config.to_json_string())
-
+    bert_config = modeling.BertConfig(vocab_size=vocab_size,
+                                      hidden_size=hidden_size,
+                                      num_hidden_layers=num_hidden_layers,
+                                      num_attention_heads=num_attention_heads,
+                                      intermediate_size=intermediate_size)
 
     tf.gfile.MakeDirs(output_dir)
 
@@ -77,98 +83,120 @@ def generate_model(output_dir,
                                 },
                                 outputs={'prob': prob})
 
-      tflite_file = os.path.join(output_dir, 'model.tflite')
-      print("exporting tflite model...")
-      converter = tf.lite.TFLiteConverter.from_session(sess, [input_ids_ph, segment_ids_ph],
-                                                        [prob])
-      if quantize:
-          converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+      # tflite_file = os.path.join(output_dir, 'model.tflite')
+      # print("exporting tflite model...")
+      # converter = tf.lite.TFLiteConverter.from_session(sess, [input_ids_ph, segment_ids_ph],
+      #                                                   [prob])
+      # if quantize:
+      #     converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
 
-      converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
-      tflite_model = converter.convert()
-      with open(tflite_file, "wb") as f:
-          f.write(tflite_model)
+      # converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
+      # tflite_model = converter.convert()
+      # with open(tflite_file, "wb") as f:
+      #     f.write(tflite_model)
 
 def convert_to_ir(model_meta_path):
-
-  command ='python -m mo_tf --output prob --disable_nhwc_to_nchw --input input_ids{i32},segment_ids{i32}' \
+  command ='python -m mo_tf --disable_nhwc_to_nchw' \
+    + ' --output prob' \
+    + ' --input input_ids{i32},segment_ids{i32}' \
+    + ' --data_type FP16' \
     + ' --input_meta_graph "' + model_meta_path + '"' \
     + ' --output_dir "' + os.path.dirname(model_meta_path) + '"'
   print(command)
   os.system(command)
 
-def generate_models():
-  hidden_size_arr = [256, 512, 768]
-  num_hidden_layers_arr = [3, 4, 6, 8, 9, 12]
-  num_attention_layers_arr = [4, 6, 8, 12, 16]
-  intermediate_size_arr = [3072] # [768, 1024, 1536, 2048, 3072]
-  experiments_dir = 'experiments'
+# def generate_models():
+#   hidden_size_arr = [256, 512, 768]
+#   num_hidden_layers_arr = [3, 4, 6, 8, 9, 12]
+#   num_attention_layers_arr = [4, 6, 8, 12, 16]
+#   intermediate_size_arr = [3072] # [768, 1024, 1536, 2048, 3072]
+#   experiments_dir = 'experiments'
 
-  model_count = 0
-  for hidden_size in hidden_size_arr:
-    for num_hidden_layers in num_hidden_layers_arr:
-      for num_attention_layers in num_attention_layers_arr:
-        for intermediate_size in intermediate_size_arr:
-          output_dir = experiments_dir + '/hidden_size_' + str(hidden_size) \
-            + '_num_hidden_layers_' + str(num_hidden_layers) \
-            + '_num_attention_layers_' + str(num_attention_layers) \
-            + '_intermediate_size_' + str(intermediate_size)
+#   model_count = 0
+#   for hidden_size in hidden_size_arr:
+#     for num_hidden_layers in num_hidden_layers_arr:
+#       for num_attention_layers in num_attention_layers_arr:
+#         for intermediate_size in intermediate_size_arr:
+#           output_dir = experiments_dir + '/hidden_size_' + str(hidden_size) \
+#             + '_num_hidden_layers_' + str(num_hidden_layers) \
+#             + '_num_attention_layers_' + str(num_attention_layers) \
+#             + '_intermediate_size_' + str(intermediate_size)
 
-          try:
-            generate_model(hidden_size, num_hidden_layers, num_attention_layers, \
-              intermediate_size, output_dir, 384, 2, True)
-          except ValueError as ve:
-            print(ve)
+#           try:
+#             generate_model(hidden_size, num_hidden_layers, num_attention_layers, \
+#               intermediate_size, output_dir, 384, 2, True)
+#           except ValueError as ve:
+#             print(ve)
 
-          # convert_to_ir(output_dir + '/model.meta')
+#           # convert_to_ir(output_dir + '/model.meta')
 
-          model_count+=1
+#           model_count+=1
 
-  print('Model count: ', model_count)
+#   print('Model count: ', model_count)
 
-def generate_models_input_size(output_experiments_dir, input_size):
-  # experiments_dir = 'experiments_input_size'
+# def generate_models_input_size(output_experiments_dir, input_size):
+#   # experiments_dir = 'experiments_input_size'
 
-  # for input_size in range(193, 400):
-  # output_dir = experiments_dir + '/input_size_' + str(input_size).zfill(3)
-  # print(output_dir)
+#   # for input_size in range(193, 400):
+#   # output_dir = experiments_dir + '/input_size_' + str(input_size).zfill(3)
+#   # print(output_dir)
 
-  output_model_dir = output_experiments_dir + '/input_size_' + str(input_size).zfill(3)
+#   output_model_dir = output_experiments_dir + '/input_size_' + str(input_size).zfill(3)
+#   try:
+#     generate_model(output_model_dir, max_seq_length = input_size)
+#   except ValueError as ve:
+#     print(ve)
+
+#   convert_to_ir(output_model_dir + '/model.meta')
+
+def init_worker():
+  signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+
+def worker(worker_id, queue):
+  model_info = queue.get()
+  model_dir_path = generated_models_dir + '/' + model_info['model_name']
+  make_dir(model_dir_path)
+  print(model_dir_path)
+
   try:
-    generate_model(output_model_dir, max_seq_length = input_size)
+    generate_model(model_dir_path,
+                  model_info['vocab_size'],
+                  model_info['hidden_size'],
+                  model_info['num_hidden_layers'],
+                  model_info['num_attention_heads'],
+                  model_info['intermediate_size'],
+                  model_info['input_size'])
   except ValueError as ve:
-    print(ve)
+    print('Error generating model: {}'.format(ve))
 
-  convert_to_ir(output_model_dir + '/model.meta')
+  convert_to_ir(model_info['model_meta_path'])
 
-import threading
-
-def generate_models_multithreaded(target_begin, target_offset):
-    print('Generating models on multiple threads\n')
-    output_experiments_dir = 'experiments_input_size'
-    thread_count = 6
-
-    # for job_idx in thread_count:
-    curr_target = target_begin
-    target_end = target_begin + target_offset
-
-    while curr_target < target_end:
-        threads = []
-
-        for thread_idx in range (0, thread_count):
-            if curr_target >= target_end:
-                break
-            threads.append(threading.Thread(target = generate_models_input_size, \
-                args = (output_experiments_dir, curr_target)))
-            curr_target += 1
-
-        for x in threads:
-            x.start()
-        for x in threads:
-            x.join()
+generated_models_dir = 'generated_models'
+num_workers = 3
 
 def main():
-  generate_models_multithreaded(400, 601 - 400)
+  make_dir(generated_models_dir)
+  manager = Manager()
+  queue = manager.Queue()
+  models_info = generate_models_info()
+  for model_info in models_info:
+    queue.put(model_info)
+  pool = Pool(num_workers, init_worker)
+
+  while not queue.empty():
+    qsize = queue.qsize()
+    workers = []
+
+    for worker_id in range(num_workers if num_workers > qsize else qsize):
+      workers.append(pool.apply_async(worker, (worker_id, queue,)))
+
+    try:
+      [worker.get() for worker in workers]
+    except:
+      print('Interrupted')
+      pool.terminate()
+      pool.join()
 
 if __name__ == '__main__':
   main()
