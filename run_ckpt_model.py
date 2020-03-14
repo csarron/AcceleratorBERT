f85f5b47847d6255c077106017b9dedda09f5217
Arron Cao <qqcao.cs@gmail.com>
2020-03-14 18:31:18 -0400

    add bert 24 models support

--- run_ckpt_model.py
+++ run_ckpt_model.py
@@ -13,6 +13,30 @@ handler = logging.StreamHandler()
 handler.setFormatter(fmt)
 logger.addHandler(handler)
 
+def init_ckpt(init_checkpoint=None, use_tpu=False):
+  tvars = tf.trainable_variables()
+  initialized_variable_names = {}
+  scaffold_fn = None
+  if init_checkpoint:
+    (assignment_map, initialized_variable_names
+    ) = modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
+    if use_tpu:
+
+      def tpu_scaffold():
+        tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+        return tf.train.Scaffold()
+
+      scaffold_fn = tpu_scaffold
+    else:
+      tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+
+  logger.info("**** Trainable Variables ****")
+  for var in tvars:
+    init_string = ""
+    if var.name in initialized_variable_names:
+      init_string = ", *INIT_FROM_CKPT*"
+    logger.info("  name = %s, shape = %s%s", var.name, var.shape,
+                    init_string)
 
 def create_model(bert_config, input_ids, segment_ids,input_mask, num_labels):
   """Creates a classification model."""
@@ -49,7 +73,7 @@ def create_model(bert_config, input_ids, segment_ids,input_mask, num_labels):
 def main(args):
     bert_config = modeling.BertConfig.from_json_file(args.bert_config_file)
     tf.gfile.MakeDirs(args.output_dir)
-    saved_model_path = os.path.join(args.output_dir, 'model_seq{}'.format(args.max_seq_length))
+    saved_model_path = os.path.join(args.output_dir, args.output_name)
     input_ids=[101, 2054, 2154, 2001, 1996, 2208, 2209, 2006, 1029, 102, 1996, 2208, 2001, 2209, 2006, 2337, 1021, 1010, 2355, 1010, 2012, 11902, 1005, 1055, 3346, 1999, 1996, 2624, 3799, 3016, 2181, 2012, 4203, 10254, 1010, 2662, 1012, 102, 0, 0]
     segment_ids=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
     input_ids = input_ids[:args.max_seq_length] + [0]*(args.max_seq_length-len(input_ids))
@@ -69,6 +93,7 @@ def main(args):
       #   eval_graph_file = saved_model_path + "_eval.pb"
       #   with open(eval_graph_file, 'w') as f:
       #     f.write(str(g.as_graph_def()))
+      init_ckpt(args.ckpt_file)
       sess.run(tf.initializers.global_variables())
 
       if export_all or args.mode == 'ckpt':
@@ -86,7 +111,7 @@ def main(args):
         logger.info('model_outputs: \n{}\n'.format(model_prob))
 
       if export_all or args.mode == 'lite':
-        tflite_file = os.path.join(args.output_dir, 'model.tflite')
+        tflite_file = os.path.join(args.output_dir, '{}.tflite'.format(args.output_name))
         logger.info("exporting tflite model...")
         converter = tf.lite.TFLiteConverter.from_session(sess, [input_ids_ph, segment_ids_ph, input_mask_ph],
                                                           [prob])
@@ -115,7 +140,9 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--bert_config_file', type=str, default=None)
-    parser.add_argument("-o", "--output_dir", type=str, default='data/bert_base')
+    parser.add_argument('-cf', '--ckpt_file', type=str, default=None)
+    parser.add_argument("-od", "--output_dir", type=str, default=None)
+    parser.add_argument("-on", "--output_name", type=str, default=None)
     parser.add_argument("-m", "--mode", type=str, choices=("lite", "ckpt", "all"),
                         default='ckpt')
     parser.add_argument("-n", "--num_labels", type=int, default=2)
