663ba58f69d6648e1798b0d39a99a750ec14c161
Arron Cao <qqcao.cs@gmail.com>
2020-03-26 01:00:58 -0400

    update

--- README.md
+++ README.md
@@ -70,3 +70,37 @@ for a in 2 4 8 12; do
   done
 done
 ````
+
+
+train models
+````bash
+2-512, 4-128
+4-512, 8-128
+6-512,10-256,12-128
+4-768,10-512
+
+# for bert 24 models
+for a in 2 4 8 12; do
+  h=$((a*64))
+  for l in $(seq 2 2 12); do
+      m=l${l}_h${h}
+      echo "running m=${m}..."
+      BERT_BASE_DIR=data/init/uncased_L-${l}_H-${h}_A-${a}
+
+      python run_classifier.py \
+        --task_name=SST \
+        --do_train=true \
+        --do_eval=true \
+        --data_dir=data/glue_data/SST-2 \
+        --vocab_file=$BERT_BASE_DIR/vocab.txt \
+        --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+        --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+        --max_seq_length=128 \
+        --train_batch_size=32 \
+        --learning_rate=2e-5 \
+        --num_train_epochs=3.0 \
+        --output_dir=data/sst_${m} 2>&1 | tee data/sst-${m}.log
+  done
+done
+
+````
--- modeling.py
+++ modeling.py
@@ -480,12 +480,12 @@ def embedding_postprocessor(input_tensor,
     # This vocab will be small so we always do one-hot here, since it is always
     # faster for a small vocabulary.
     flat_token_type_ids = tf.reshape(token_type_ids, [-1])
-    token_type_embeddings = tf.gather(token_type_table, flat_token_type_ids)
-    # one_hot_ids = tf.one_hot(flat_token_type_ids, depth=token_type_vocab_size)
+    # token_type_embeddings = tf.gather(token_type_table, flat_token_type_ids)
+    one_hot_ids = tf.one_hot(flat_token_type_ids, depth=token_type_vocab_size)
 
-    # token_type_embeddings = tf.matmul(one_hot_ids, token_type_table)
-    # token_type_embeddings = tf.reshape(token_type_embeddings,
-    #                                    [batch_size, seq_length, width])
+    token_type_embeddings = tf.matmul(one_hot_ids, token_type_table)
+    token_type_embeddings = tf.reshape(token_type_embeddings,
+                                       [batch_size, seq_length, width])
     output += token_type_embeddings
 
   if use_position_embeddings:
@@ -540,8 +540,8 @@ def create_attention_mask_from_input_mask(from_tensor, to_mask):
   to_shape = get_shape_list(to_mask, expected_rank=2)
   to_seq_length = to_shape[1]
 
-  # to_mask = tf.cast(tf.reshape(to_mask, [batch_size, 1, to_seq_length]), tf.float32)
-  to_mask = tf.reshape(to_mask, [batch_size, 1, to_seq_length])
+  to_mask = tf.cast(tf.reshape(to_mask, [batch_size, 1, to_seq_length]), tf.float32)
+  # to_mask = tf.reshape(to_mask, [batch_size, 1, to_seq_length])
 
   # We don't assume that `from_tensor` is a mask (although it could be). We
   # don't actually care if we attend *from* padding tokens (only *to* padding)
