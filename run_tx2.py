57273753480182eb6608507e83c0e3e4712b0fcc
Arron Cao <qqcao.cs@gmail.com>
2020-06-30 00:33:56 -0400

    support 24 bert models

--- run_tx2.py
+++ run_tx2.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import os
 import time
 from collections import defaultdict
@@ -80,8 +81,27 @@ def create_model(bert_config, input_ids, segment_ids, input_mask, num_labels):
     return probabilities
 
 
+def gen_config():
+    for num_hidden_layers in [2, 4, 6, 8, 10, 12]:
+        for num_attention_heads in [2, 4, 8, 12]:
+            hidden_size = 64 * num_attention_heads
+            bert_config = modeling.BertConfig(
+                vocab_size=30522,
+                hidden_size=hidden_size,
+                num_hidden_layers=num_hidden_layers,
+                num_attention_heads=num_attention_heads,
+                intermediate_size=4 * hidden_size,
+                hidden_act="gelu",
+                hidden_dropout_prob=0.1,
+                attention_probs_dropout_prob=0.1,
+                max_position_embeddings=512,
+                type_vocab_size=2,
+                initializer_range=0.02)
+            yield bert_config
+
+
 def main(args):
-    bert_config = modeling.BertConfig.from_json_file(args.bert_config_file)
+    # bert_config = modeling.BertConfig.from_json_file(args.bert_config_file)
     # tf.gfile.MakeDirs(args.output_dir)
     input_ids = [101, 2054, 2154, 2001, 1996, 2208, 2209, 2006, 1029, 102, 1996,
                  2208, 2001, 2209, 2006, 2337, 1021, 1010, 2355, 1010, 2012,
@@ -100,68 +120,86 @@ def main(args):
         device = 'GPU'
         print("using GPU, Available: ",
               len(tf.config.experimental.list_physical_devices('GPU')))
-    with tf.device('/{}:*'.format(device)):
-        sess = tf.Session()
-        input_ids_ph = tf.placeholder(shape=[1, args.max_seq_length],
-                                      dtype=tf.int32, name='input_ids')
-        segment_ids_ph = tf.placeholder(shape=[1, args.max_seq_length],
-                                        dtype=tf.int32, name='segment_ids')
-        input_mask_ph = tf.placeholder(shape=[1, args.max_seq_length],
-                                       dtype=tf.float32, name='input_mask')
-        prob = create_model(bert_config, input_ids_ph, segment_ids_ph,
-                            input_mask_ph, args.num_labels)
-        init_ckpt(args.ckpt_file)
-        sess.run(tf.initializers.global_variables())
+    # results = []
+    fields = ['L', 'H', 'A', 'latency'] + ['energy_{}'.format(n[0])
+                                           for n in getNodes()]
+    with tf.device('/{}:*'.format(device)), open(args.output_file, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=fields)
+        writer.writeheader()
+        for bert_config in gen_config():
+            tf.reset_default_graph()
+            sess = tf.Session()
+            input_ids_ph = tf.placeholder(shape=[1, args.max_seq_length],
+                                          dtype=tf.int32, name='input_ids')
+            segment_ids_ph = tf.placeholder(shape=[1, args.max_seq_length],
+                                            dtype=tf.int32, name='segment_ids')
+            input_mask_ph = tf.placeholder(shape=[1, args.max_seq_length],
+                                           dtype=tf.float32, name='input_mask')
+            prob = create_model(bert_config, input_ids_ph, segment_ids_ph,
+                                input_mask_ph, args.num_labels)
+            init_ckpt(args.ckpt_file)
+            sess.run(tf.initializers.global_variables())
 
-        input_ids = np.reshape(input_ids, [1, args.max_seq_length])
-        segment_ids = np.reshape(segment_ids, [1, args.max_seq_length])
-        input_mask = input_ids.astype(np.bool).astype(np.float32)
-        # warm_up 2 rounds
-        for _ in range(2):
-            model_prob = sess.run(prob, {
-                input_ids_ph: input_ids,
-                segment_ids_ph: segment_ids,
-                input_mask_ph: input_mask,
-            })
-        logger.info('start benchmarking, model_prob: \n{}\n'.format(model_prob))
-
-        infer_times = []
-        infer_energy = defaultdict(list)
-        for iteration in range(args.iterations):
-            pl = PowerLogger(interval=0.05, nodes=getNodes())
-            pl.start()
-            pl.recordEvent('run model!')
-            start = time.perf_counter()
-            passes = 10
-            for i in range(passes):
+            input_ids = np.reshape(input_ids, [1, args.max_seq_length])
+            segment_ids = np.reshape(segment_ids, [1, args.max_seq_length])
+            input_mask = input_ids.astype(np.bool).astype(np.float32)
+            # warm_up 2 rounds
+            for _ in range(2):
                 model_prob = sess.run(prob, {
                     input_ids_ph: input_ids,
                     segment_ids_ph: segment_ids,
                     input_mask_ph: input_mask,
                 })
-            inference_time = (time.perf_counter() - start) / passes
-            pl.stop()
-            iteration_energy = pl.getEnergyTraces()
-            for energy_name, energy_val in iteration_energy.items():
-                infer_energy[energy_name].append(energy_val / passes)
-                logger.info('{}_energy iter{} {:.1f} J'.format(
-                    energy_name, iteration + 1, + energy_val / passes))
-            infer_times.append(inference_time * 1000)
-            logger.info('latency iter{} {:.1f}ms'.format(
-                iteration + 1, inference_time * 1000))
-        logger.info('{}: latency_avg={:.1f} ms, std={:.3f} ms'.format(
-            device, np.mean(infer_times), np.std(infer_times)))
-        for k, v in infer_energy.items():
-            logger.info('{}: {}_energy_avg={:.1f} J, std={:.3f} J'.format(
-                device, k, np.mean(v), np.std(v)))
+            logger.info(
+                'start benchmarking, model_prob: \n{}\n'.format(model_prob))
+
+            infer_times = []
+            infer_energy = defaultdict(list)
+            for iteration in range(args.iterations):
+                pl = PowerLogger(interval=0.05, nodes=getNodes())
+                pl.start()
+                pl.recordEvent('run model!')
+                start = time.perf_counter()
+                passes = 10
+                for i in range(passes):
+                    model_prob = sess.run(prob, {
+                        input_ids_ph: input_ids,
+                        segment_ids_ph: segment_ids,
+                        input_mask_ph: input_mask,
+                    })
+                inference_time = (time.perf_counter() - start) / passes
+                pl.stop()
+                iteration_energy = pl.getEnergyTraces()
+                for energy_name, energy_val in iteration_energy.items():
+                    infer_energy[energy_name].append(energy_val / passes)
+                    logger.info('{}_energy iter{} {:.3f} J'.format(
+                        energy_name, iteration + 1, + energy_val / passes))
+                infer_times.append(inference_time * 1000)
+                logger.info('latency iter{} {:.3f}ms'.format(
+                    iteration + 1, inference_time * 1000))
+            latency_avg = np.mean(infer_times)
+            latency_std = np.std(infer_times)
+            logger.info('{}: latency_avg={:.3f} ms, std={:.3f} ms'.format(
+                device, latency_avg, latency_std))
+            record = {'L': bert_config.num_hidden_layers,
+                      'H': bert_config.hidden_size,
+                      'A': bert_config.num_attention_heads,
+                      'latency': latency_avg}
+            for k, v in infer_energy.items():
+                logger.info('{}: {}_energy_avg={:.3f} J, std={:.3f} J'.format(
+                    device, k, np.mean(v), np.std(v)))
+                record['energy_{}'.format(k)] = np.mean(v)
+            # results.append(record)
+            writer.writerow(record)
+            f.flush()
+    print('all done')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--bert_config_file', type=str, default=None)
     parser.add_argument('-cf', '--ckpt_file', type=str, default=None)
-    parser.add_argument("-od", "--output_dir", type=str, default=None)
-    parser.add_argument("-on", "--output_name", type=str, default=None)
+    parser.add_argument("-of", "--output_file", type=str, default=None)
     parser.add_argument("-i", "--iterations", type=int, default=10)
     parser.add_argument("-n", "--num_labels", type=int, default=2)
     parser.add_argument("-l", "--max_seq_length", type=int, default=128)
