38fead9ef79ffcf4297c614888ba339cf57ce706
Alexandru Irimiea <alexandru.irim@gmail.com>
2020-08-10 02:43:19 +0300

    Cleaned run_faster_rcnn.py

--- run_faster_rcnn.py
+++ run_faster_rcnn.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
-import re
-import sys
 import os
-from argparse import ArgumentParser, SUPPRESS
+import sys
 import time
 import numpy as np
-import threading
-import signal
-import json
-from datetime import datetime
-from datetime import timedelta
-import tensorflow as tf
 import logging
 log = logging.getLogger('eet')
-import cv2
-
 from openvino.inference_engine import IENetwork, IECore#, IEPlugin
+from argparse import ArgumentParser, SUPPRESS
 
 log.setLevel(logging.INFO)
 fmt = logging.Formatter("%(levelname)s:%(asctime)s.%(msecs)03d:%(pathname)s:%(lineno)d: %(message)s",
@@ -28,25 +19,38 @@ log.propagate = False
 # To convert the tf model to OpenVINO, go to faster_rcnn_resnet101_coco_2018_01_28 directory and execute:
 # python -m mo_tf --input_model frozen_inference_graph.pb --output_dir . --tensorflow_use_custom_operations_config "C:\Program Files (x86)\IntelSWTools\openvino\deployment_tools\model_optimizer\extensions\front\tf\faster_rcnn_support.json" --tensorflow_object_detection_api_pipeline_config ./pipeline.config --reverse_input_channels --data_type FP16 --input_shape [1,600,1024,3]
 
-xml_path = 'faster_rcnn_resnet101_coco_2018_01_28/frozen_inference_graph.xml'
-bin_path = os.path.splitext(xml_path)[0] + ".bin"
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group('Options')
+    args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
+    args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model.", required=True,
+                      type=str)
+    return parser
 
-ie = IECore()
-network = ie.read_network(model=xml_path, weights=bin_path)
-network.add_outputs("detection_output")
+def main():
+    args = build_argparser().parse_args()
+    xml_path = args.model
+    bin_path = os.path.splitext(xml_path)[0] + ".bin"
 
-input_blob = next(iter(network.inputs))
-out_blob = next(iter(network.outputs))
-network.batch_size = 1
+    ie = IECore()
+    network = ie.read_network(model=xml_path, weights=bin_path)
+    # network.add_outputs("detection_output")
 
-# n, c,
-h, w = network.inputs[input_blob].shape
-images = np.ndarray(shape=(h, w))
+    input_blob = next(iter(network.inputs))
+    out_blob = next(iter(network.outputs))
+    network.batch_size = 1
 
-executable_network = ie.load_network(network=network, device_name='MYRIAD')
+    # n, c, h, w = network.inputs[input_blob].shape
+    # images = np.ndarray(shape=(h, w))
+    images = np.ndarray(shape=network.inputs[input_blob].shape)
 
-start = time.perf_counter()
-res = executable_network.infer(inputs={input_blob: images})
-inference_time = time.perf_counter() - start
+    executable_network = ie.load_network(network=network, device_name='MYRIAD')
 
-log.info('Latency {:.1f} ms'.format(inference_time * 1000))
+    start = time.perf_counter()
+    res = executable_network.infer(inputs={input_blob: images})
+    inference_time = time.perf_counter() - start
+
+    log.info('Latency {:.1f} ms'.format(inference_time * 1000))
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
